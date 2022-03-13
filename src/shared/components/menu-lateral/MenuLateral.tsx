559712e3a2a9193c9770b10734c446a6f1825635
FRANCISCO OLIVEIRA <fransoliveira@gmail.com>
2022-03-13 20:02:35 -0300

    feat: Material-UI drawer responsivo

--- src/shared/components/menu-lateral/MenuLateral.tsx
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -1,5 +1,11 @@
-import { Drawer, useTheme, Avatar } from "@mui/material";
+import { Drawer, useTheme, Avatar, Divider, Icon } from "@mui/material";
 import { Box } from "@mui/system";
+import List from "@mui/material/List";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+
+
 
 export const MenuLateral: React.FC = ({ children }) => {
   const theme = useTheme();
@@ -7,7 +13,12 @@ export const MenuLateral: React.FC = ({ children }) => {
   return (
     <>
       <Drawer open={true} variant="permanent">
-        <Box width={theme.spacing(28)}>
+        <Box
+          width={theme.spacing(28)}
+          height="100%"
+          display="flex"
+          flexDirection="column"
+        >
           <Box
             width="100%"
             height={theme.spacing(20)}
@@ -20,6 +31,18 @@ export const MenuLateral: React.FC = ({ children }) => {
               alt="Olifrans"
               src="https://media-exp1.licdn.com/dms/image/C4E03AQHQO_-QbxYi8Q/profile-displayphoto-shrink_100_100/0/1632957241631?e=1652918400&v=beta&t=9z1zIxkYa0Vys0sv5aEjVHExNLLfK8Hh5Ku3EMUbMgw"
             />
+          </Box>
+
+          <Divider />
+          <Box flex={1}>
+            <List component="nav">
+              <ListItemButton>
+                <ListItemIcon>
+                    <Icon>home</Icon>                  
+                </ListItemIcon>
+                <ListItemText primary="Home" />
+              </ListItemButton>
+            </List>
           </Box>
         </Box>
       </Drawer>
