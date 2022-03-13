5040378db425117d732808eaa5a5b1fba0f040e6
FRANCISCO OLIVEIRA <fransoliveira@gmail.com>
2022-03-13 14:24:55 -0300

    feat: ligth theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { BrowserRouter } from "react-router-dom";
 import { AppRouter } from "./routes";
+import { ThemeProvider } from "@mui/material";
+import { LightTheme } from "./shared/themes";
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <AppRouter />
-        Olifrans Olifrans
-      </div>
-    </BrowserRouter>
+    <ThemeProvider theme={LightTheme}>
+      <BrowserRouter>
+        <div className="App">
+          <AppRouter />
+          Olifrans Olifrans
+        </div>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 };
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route, Navigate } from "react-router-dom";
+import { Button } from "@mui/material";
 
 export const AppRouter = () => {
   return (
     <Routes>
-      <Route path="/pagina-inicial" element={<p>Home</p>} />
+      <Route path="/pagina-inicial" element={<Button variant="contained" color="primary">Teste</Button>} />
+      {/* <Route path="/pagina-inicial" element={<p>Home</p>} /> */}
 
-      <Route path="*" element={<Navigate to="/pagina-inicial"/>} />
+      <Route path="*" element={<Navigate to="/pagina-inicial" />} />
     </Routes>
   );
 };
