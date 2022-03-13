ac44370ac1cb35afe1697bfa0c819c0603eb9fec
FRANCISCO OLIVEIRA <fransoliveira@gmail.com>
2022-03-13 16:09:42 -0300

    feat: Troca de theme-toggleTheme e new theme dark

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
 import { BrowserRouter } from "react-router-dom";
 import { AppRouter } from "./routes";
-import { ThemeProvider } from "@mui/material";
-import { LightTheme } from "./shared/themes";
+import { AppThemeProvider } from "./shared/contexts";
 
 export const App = () => {
   return (
-    <ThemeProvider theme={LightTheme}>
+    <AppThemeProvider>
       <BrowserRouter>
         <div className="App">
           <AppRouter />
           Olifrans Olifrans
         </div>
       </BrowserRouter>
-    </ThemeProvider>
+    </AppThemeProvider>
   );
 };
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,19 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import { Button } from "@mui/material";
+import { useAppThemeContext } from "../shared/contexts";
 
 export const AppRouter = () => {
+  const { toggleTheme } = useAppThemeContext();
   return (
     <Routes>
-      <Route path="/pagina-inicial" element={<Button variant="contained" color="primary">Teste</Button>} />
+      <Route
+        path="/pagina-inicial"
+        element={
+          <Button variant="contained" color="primary" onClick={toggleTheme}>
+            toggleTheme
+          </Button>
+        }
+      />
       {/* <Route path="/pagina-inicial" element={<p>Home</p>} /> */}
 
       <Route path="*" element={<Navigate to="/pagina-inicial" />} />
