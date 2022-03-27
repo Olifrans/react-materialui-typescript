f2a3847bd528739de69983e7124802c736cc98b1
FRANCISCO OLIVEIRA <fransoliveira@gmail.com>
2022-03-27 16:00:10 -0300

    feat: Incons(Salvar e voltar para Salvar e fechar)

--- src/shared/components/ferramentas-de-detalhe/FerramentasDeDetalhe.tsx
+++ src/shared/components/ferramentas-de-detalhe/FerramentasDeDetalhe.tsx
@@ -106,7 +106,7 @@ export const FerramentasDeDetalhe: React.FC<IFerramentasDeDetalheProps> = ({
             textOverflow="ellipsis"
             overflow="hidden"
           >
-              Salvar e voltar
+              Salvar e fechar
           </Typography>
         </Button>
       )}
